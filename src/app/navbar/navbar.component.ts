c862254178bb9e0d1ae0c7d9e58890ee0db24d52
Justin Niles <justin.niles12@gmail.com>
2016-09-05 16:48:11 -0700

    Add working navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,26 @@
 import { Component } from "@angular/core";
 
-interface NavbarItems {
+interface NavbarItem {
   name: string;
   url: string;
 }
 
 @Component({
-  selector: "navbar",
+  selector: ".tv-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: [ "./navbar.component.scss" ]
 })
 export class NavbarComponent {
 
-  navbarItems: NavbarItems[] = [
-
+  navbarItems: NavbarItem[] = [
+    {
+      name: "Home",
+      url: "/home"
+    },
+    {
+      name: "Admin",
+      url: "/admin"
+    }
   ];
 
   constructor() {
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { NavbarComponent } from "./navbar.component";
 
 @NgModule({
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterModule ],
   declarations: [ NavbarComponent ],
   exports: [ NavbarComponent ]
 })
